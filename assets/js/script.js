2edd7231aa0e57b7bdb809ea878a5420ab38d456
Nazmul Hossain <nazmulhasannh4082041@gmail.com>
2026-07-25 04:01:27 +0600

    all page add and chang

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 
-// news 
+// Current Date & Time
+function updateClock() {
+    const now = new Date();
+
+    // Format Options
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+    // Update Card Text
+    document.getElementById('date').textContent = now.toLocaleDateString(undefined, dateOptions);
+    document.getElementById('time').textContent = now.toLocaleTimeString(undefined, timeOptions);
+}
+
+// Run immediately, then repeat every 1000ms (1 second)
+updateClock();
+setInterval(updateClock, 1000);
+
+
+
+
+// Ticker
 $(document).ready(function () {
 
-    $('#newsTicker').newsTicker({
-        row_height: 45,
-        max_rows: 1,
-        duration: 3000
-    });
-
+    var $ticker = $('#newsTicker');
+    var $tickerContents = $ticker.html();
+    $ticker.append($tickerContents);
 });
-
